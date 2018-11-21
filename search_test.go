9a969f67cb4ca6058e1ad52ba7e2eb03666457ce
BeanWei <157318439@qq.com>
2018-11-21 08:40:03 +0800

    :sparkles: 搜索测试完成

--- search_test.go
+++ search_test.go
@@ -5,12 +5,17 @@ import (
 	"testing"
 )
 
+const testAll, index = false, 1
+
 func Test_search(t *testing.T) {
-	for i, s := range sites {
-		if i == 2 {
+	if testAll {
+		for _, s := range sites {
 			resp := search("hello", s, map[string]int{"limit": 10})
 			fmt.Println(resp)
 		}
+	} else {
+		resp := search("hello", sites[index], map[string]int{"limit": 10})
+		fmt.Println(resp)
 	}
 
 }
