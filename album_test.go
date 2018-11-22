a757d3c30eb7d3fa3c9ec36bdaca8a2cb087db12
BeanWei <157318439@qq.com>
2018-11-22 14:47:29 +0800

    :hammer: 专辑获取测试完成

--- album_test.go
+++ album_test.go
@@ -10,20 +10,24 @@ const testAll4album, index4album = false, 0
 func Test_album(t *testing.T) {
 
 	albumID := map[string]string{
-		"netease": "",
-		"tencent": "",
-		"xiami":   "",
-		"kugou":   "",
-		"baidu":   ""}
+		"netease": "35501199",
+		"tencent": "003rytri2FHG3V",
+		"xiami":   "9cGicX47eb8",
+		"kugou":   "1645030",
+		"baidu":   "541223882"}
 
 	if testAll4album {
 		for _, s := range sites {
-			resp := song(s, albumID[s])
+			resp := album(s, albumID[s])
 			fmt.Println(resp)
 		}
 	} else {
-		resp := song(sites[index4album], albumID[sites[index4album]])
+		resp := album(sites[index4album], albumID[sites[index4album]])
 		fmt.Println(resp)
 	}
 
 }
+
+/*
+TODO: 虾米专辑详情接口容易背ban，待完善。
+*/
